1f2df7c2c66de742439ee4426ab29b0d51de544a
Parth Bhanushali <52828668+Parth-Bhanushali@users.noreply.github.com>
2024-10-28 20:29:54 +0530

    Prevent render from spinning down

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import startPingJob from "./cron/ping";
 import connectDatabase from "./config/db";
 import { authRoutes, chatRoutes, userRoutes } from "./routes";
 
@@ -7,10 +8,15 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+startPingJob();
 connectDatabase();
 
 app.use("/api/auth", authRoutes);
 app.use("/api/chat", chatRoutes);
 app.use("/api/users", userRoutes);
+
+app.get('/', (req, res) => {
+	res.send('This url serves the backend for chat-app-frontend');
+});
 
 export default app;
